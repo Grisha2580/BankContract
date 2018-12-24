5c323b3e08012c9fe35ff6668e9dd1ca940453e7
Grisha <grish2580@gmail.com>
2018-12-23 22:33:42 -0500

    first commit

---
+++ Bank.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.25;
+contract Bank {
+    address owner;
+    mapping(address => uint) balances;
+    
+    function Bank() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        if (balances[msg.sender] >= amount) {
+            balances[msg.sender] -= amount;
+            msg.sender.transfer(amount);
+        }
+    }
+
+    function getMyBalance() public view returns(uint) {
+        return balances[msg.sender];
+    }
+
+    function kill() public {
+        if (msg.sender == owner)
+            selfdestruct(owner);
+    }
+}
